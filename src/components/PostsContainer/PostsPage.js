438453d5f4bc7381705c588613097a4586b559a9
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-18 13:03:08 -0700

    importing data

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,14 +4,16 @@ import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-import dummyDate from "../../dummy-data"; //how did they find this route? This is umporting that dummy data
+import dummyDate from “../../dummy-data”;
 const PostsPage = () => {
   // set up state for your data
-  
+  // const [state, ] = useState(dummyData)
+  // console.log(state)
   
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+     
     </div>
   );
 };
