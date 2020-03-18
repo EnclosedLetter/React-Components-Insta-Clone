da68f5af855cc725c84bab18fb78994e95f77af4
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-18 12:52:00 -0700

    fixed likes

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,13 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-  const [comments, ] = useState(props.comments)
+  const [comments, ] = useState(props.comments); //WHERE DOES PROP COME FROM? 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map(comment => { //GET THE useState of comments var name, THEN MAP THROUGH THAT
+        return <Comment comment={comment} />
+      })}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,12 +1,14 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-
+import dummyDate from "../../dummy-data"; //how did they find this route? This is umporting that dummy data
 const PostsPage = () => {
   // set up state for your data
+  
+  
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
